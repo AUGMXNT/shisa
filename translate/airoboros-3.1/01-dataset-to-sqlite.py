cd94ea7b596e998d7abb3ddf9c39dab8d83ec6ac
lhl <lhl@randomfoo.net>
2023-10-28 18:52:21 +0900

    grab dataset to sqlite db

---
+++ translate/airoboros-3.1/01-dataset-to-sqlite.py
@@ -0,0 +1,37 @@
+from   datasets import load_dataset
+import json
+import sqlite3
+
+# Load Sqlite DB
+conn = sqlite3.connect("airoboros.db")
+c = conn.cursor()
+c.execute("""
+CREATE TABLE IF NOT EXISTS airoboros_31 (
+    id VARCHAR PRIMARY KEY,
+    category VARCHAR,
+    conversation TEXT,
+    conversation_ja TEXT
+);
+""")
+conn.commit()
+
+ds = load_dataset('jondurbin/airoboros-3.1')
+i = 0
+print(ds)
+# Insert items into SQLite table
+for item in ds['train']:
+    print(f"{i:>5} : {item['id']} : {item['category']}")
+    i+=1
+    # Check if the id already exists in the table
+    c.execute("SELECT id FROM airoboros_31 WHERE id = ?", (item['id'],))
+    if c.fetchone() is None:
+        # JSON-encode the conversations
+        conv_json = json.dumps(item['conversations'])
+        
+        # Insert new row
+        c.execute("INSERT INTO airoboros_31 (id, category, conversation) VALUES (?, ?, ?)",
+                  (item['id'], item['category'], conv_json))
+        conn.commit()
+
+# Commit the changes and close the connection
+conn.close()
