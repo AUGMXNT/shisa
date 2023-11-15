5a492e2b675ff9a639a8de89317b4df1e05f9c00
Jon Durbin <jon@jondurbin.com>
2023-11-15 04:52:20 -0500

    Update 04-add-bench-train.py

--- translate/ultraboros/04-add-bench-train.py
+++ translate/ultraboros/04-add-bench-train.py
@@ -2,7 +2,6 @@ from datasets import Dataset, concatenate_datasets
 import glob
 import json
 import sqlite3
-import re
 import uuid
 
 # Load Sqlite DB
@@ -11,8 +10,10 @@ c = conn.cursor()
 
 # Load the tuning/training portions of the datasets.
 datasets = []
-for path in glob.glob('dataset_dir/tuning/*.json'):
-    dataset = Dataset.from_json(path).train_test_split(train_size=0.05, shuffle=False, seed=42)["train"]
+for path in glob.glob("dataset_dir/tuning/*.json"):
+    dataset = Dataset.from_json(path).train_test_split(
+        train_size=0.05, shuffle=False, seed=42
+    )["train"]
     datasets.append(dataset)
 dataset = concatenate_datasets(datasets)
 
@@ -25,23 +26,34 @@ for item in dataset:
         },
         {
             "from": "human",
-            "value": "\n\n".join([
-                "以下はタスクを説明する指示と、追加の背景情報を提供する入力の組み合わせです。要求を適切に満たす回答を書いてください。",
-                "\n".join([
-                    "### 指示：",
-                    item["instruction"],
-                ]),
-                "" if not item["input"].strip() else "\n".join([
-                    "### 入力：",
-                    item["input"],
-                ]),
-                "### 回答：",
-            ]),
+            "value": "\n\n".join(
+                [
+                    "以下はタスクを説明する指示と、追加の背景情報を提供する入力の組み合わせです。要求を適切に満たす回答を書いてください。",
+                    "\n".join(
+                        [
+                            "### 指示：",
+                            item["instruction"],
+                        ]
+                    ),
+                    ""
+                    if not item["input"].strip()
+                    else "\n".join(
+                        [
+                            "### 入力：",
+                            item["input"],
+                        ]
+                    ),
+                    "### 回答：",
+                ]
+            ),
         },
         {
             "from": "gpt",
             "value": item["output"],
         },
     ]
-    c.execute("INSERT INTO ultraboros (id, source, conversation_ja) VALUES (?, ?, ?) ON CONFLICT DO NOTHING", (_id, "lm-jp-eval", json.dumps(conversation)))
+    c.execute(
+        "INSERT INTO ultraboros (id, source, conversation_ja) VALUES (?, ?, ?) ON CONFLICT DO NOTHING",
+        (_id, "lm-jp-eval", json.dumps(conversation)),
+    )
     conn.commit()
