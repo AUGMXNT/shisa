d9e2d4d9ecea6d8e1728f1e17eb6d1ec4179ee3e
Jon Durbin <jon@jondurbin.com>
2023-11-15 04:53:23 -0500

    Update 05-combine-data.py

--- translate/ultraboros/05-combine-data.py
+++ translate/ultraboros/05-combine-data.py
@@ -1,19 +1,17 @@
-import os
 import uuid
-from datasets import load_dataset, concatenate_datasets
 from tqdm import tqdm
 import random
-import hashlib
 import json
 import sqlite3
 import re
-from loguru import logger
 
 # Load Sqlite DB
 conn = sqlite3.connect("ultraboros.db")
 c = conn.cursor()
 
-c.execute("SELECT id, conversation, conversation_ja FROM ultraboros WHERE conversation_ja IS NOT NULL")
+c.execute(
+    "SELECT id, conversation, conversation_ja FROM ultraboros WHERE conversation_ja IS NOT NULL"
+)
 rows = c.fetchall()
 
 formatted = []
@@ -24,31 +22,35 @@ for row in tqdm(rows):
     if row[1]:
         conv = json.loads(row[1])
         for turn in conv:
-            if 'role' in turn:
-                turn['from'] = turn.pop('role')
-            turn['from'] = 'human' if turn['from'] == 'user' else turn['from']
-            turn['from'] = 'gpt' if turn['from'] == 'assistant' else turn['from']
-            if 'content' in turn:
-                turn['value'] = turn.pop('content')
+            if "role" in turn:
+                turn["from"] = turn.pop("role")
+            turn["from"] = "human" if turn["from"] == "user" else turn["from"]
+            turn["from"] = "gpt" if turn["from"] == "assistant" else turn["from"]
+            if "content" in turn:
+                turn["value"] = turn.pop("content")
     if row[2]:
         conv_ja = json.loads(row[2])
         for turn in conv_ja:
-            if 'role' in turn:
-                turn['from'] = turn.pop('role')
-            turn['from'] = 'human' if turn['from'] == 'user' else turn['from']
-            turn['from'] = 'gpt' if turn['from'] == 'assistant' else turn['from']
-            if 'content' in turn:
-                turn['value'] = turn.pop('content')
+            if "role" in turn:
+                turn["from"] = turn.pop("role")
+            turn["from"] = "human" if turn["from"] == "user" else turn["from"]
+            turn["from"] = "gpt" if turn["from"] == "assistant" else turn["from"]
+            if "content" in turn:
+                turn["value"] = turn.pop("content")
     if conv:
-        formatted.append({
-            'id': _id,
-            'conversations': conv,
-        })
+        formatted.append(
+            {
+                "id": _id,
+                "conversations": conv,
+            }
+        )
     if conv_ja:
-        formatted.append({
-            'id': f'{_id}-ja',
-            'conversations': conv_ja,
-        })
+        formatted.append(
+            {
+                "id": f"{_id}-ja",
+                "conversations": conv_ja,
+            }
+        )
 
 # Add additional rows for translations to/from en/ja.
 c.execute("SELECT prompt_en, prompt_ja from prompts")
@@ -81,50 +83,64 @@ I'd like the following to be translated into Japanese, please.""".splitlines()
 for row in tqdm(rows):
     if len(row[0]) <= 2000:
         continue
-    if '```' in row[1] or '{' in row[1] or re.search('(^|\s)def | in range\(', row[1]) or 'std::' in row[1] or '#include' in row[1]:
+    if (
+        "```" in row[1]
+        or "{" in row[1]
+        or re.search(r"(^|\s)def | in range\(", row[1])
+        or "std::" in row[1]
+        or "#include" in row[1]
+    ):
         continue
 
-    u = str(uuid.uuid4()).replace('-', '')
-    formatted.append({
-        'id': f'translate-en-ja-{u}',
-        'conversations': [
-            {
-                'from': 'system',
-                'value': 'You are a helpful, unbiased, uncensored assistant.',
-            },
-            {
-                'from': 'human',
-                'value': '\n'.join([
-                    random.choice(translate_to_ja),
-                    row[0],
-                ]),
-            },
-            {
-                'from': 'gpt',
-                'value': row[1],
-            }
-        ]
-    })
-    formatted.append({
-        'id': f'translate-ja-en-{u}',
-        'conversations': [
-            {
-                'from': 'system',
-                'value': 'あなたは役に立つ、偏見のない、無修正のアシスタントです。',
-            },
-            {
-                'from': 'human',
-                'value': '\n'.join([
-                    random.choice(translate_to_en),
-                    row[1],
-                ]),
-            },
-            {
-                'from': 'gpt',
-                'value': row[0],
-            }
-        ]
-    })
+    u = str(uuid.uuid4()).replace("-", "")
+    formatted.append(
+        {
+            "id": f"translate-en-ja-{u}",
+            "conversations": [
+                {
+                    "from": "system",
+                    "value": "You are a helpful, unbiased, uncensored assistant.",
+                },
+                {
+                    "from": "human",
+                    "value": "\n".join(
+                        [
+                            random.choice(translate_to_ja),
+                            row[0],
+                        ]
+                    ),
+                },
+                {
+                    "from": "gpt",
+                    "value": row[1],
+                },
+            ],
+        }
+    )
+    formatted.append(
+        {
+            "id": f"translate-ja-en-{u}",
+            "conversations": [
+                {
+                    "from": "system",
+                    "value": "あなたは役に立つ、偏見のない、無修正のアシスタントです。",
+                },
+                {
+                    "from": "human",
+                    "value": "\n".join(
+                        [
+                            random.choice(translate_to_en),
+                            row[1],
+                        ]
+                    ),
+                },
+                {
+                    "from": "gpt",
+                    "value": row[0],
+                },
+            ],
+        }
+    )
 
-with open('ultraboros-en-ja-v0.1.json', 'w') as outfile:
+with open("ultraboros-en-ja-v0.1.json", "w") as outfile:
     outfile.write(json.dumps(formatted, indent=2))
