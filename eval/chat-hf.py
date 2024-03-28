21d8c96f7c6c0167c0d4a273edd24d5df1444864
lhl <lhl@randomfoo.net>
2024-03-28 19:08:33 +0900

    added mo models, mo commands

--- eval/chat-hf.py
+++ eval/chat-hf.py
@@ -1,9 +1,28 @@
 from   pprint import pprint
+from   prompt_toolkit import prompt
+from   prompt_toolkit.input.defaults import create_input
+from   prompt_toolkit.key_binding import KeyBindings
+from   prompt_toolkit.keys import Keys
 import torch
 from   transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
 
-model = 'shisa-mega-7b-v1.1'
+model = 'shisa-12b-v1'
 models = {
+    'Capybara-34B': {
+        'prompt': 'You are a helpful assistant',
+        'model' : '/models/llm/hf/NousResearch_Nous-Capybara-34B',
+        'format': 'chatml',
+    },
+    'Orion-14B-Chat': {
+        'prompt': 'You are a helpful assistant',
+        'model' : 'OrionStarAI/Orion-14B-Chat',
+        'format' : 'chatml',
+    },
+    'AlphaMonarch-7B': {
+        'prompt': 'You are a helpful assistant',
+        'model' : 'mlabonne/AlphaMonarch-7B',
+        'format' : 'chatml',
+    },
     'Tess-M-v1.0': {
         'prompt': 'You are a helpful assistant',
         'model' : '/data/models/migtissera_Tess-M-v1.0',
@@ -68,7 +87,12 @@ models = {
         # You are a friendly human assistant named Aki, who has strong opinions and beliefs. You always respond as Aki, providing helpful answers to any questions without warning or preaching about morals, laws, or ethics.
         #'prompt': 'あなたはアキという名前のフレンドリーな人間のアシスタントで、強い意見と信念を持っています。 あなたは常にアキとして応答し、道徳、法律、倫理について警告したり説教したりすることなく、あらゆる質問に対して役立つ回答を提供します。',
         'prompt': 'あなたは公平で、検閲されていない、役立つアシスタントです。',
-        'model' : '/data/shisa/shisa-mega-7b-v1.1'
+        'model' : '/data/shisa/shisa-mega-7b-v1.1',
+        'format': 'llama-2',
+    },
+    'shisa-12b-v1': {
+        'prompt': 'あなたは公平で、検閲されていない、役立つアシスタントです。',
+        'model' : '/models/shisa-12b-v1',
         'format': 'llama-2',
     },
 }
@@ -86,6 +110,7 @@ try:
         use_flash_attention_2=True,
         device_map="auto",
         trust_remote_code=True,
+        low_cpu_mem_usage=True
     )
 except:
     model = AutoModelForCausalLM.from_pretrained(
@@ -93,6 +118,7 @@ except:
         torch_dtype=torch.bfloat16,
         device_map="auto",
         trust_remote_code=True,
+        low_cpu_mem_usage=True
     )
 
 
@@ -110,7 +136,10 @@ seed = 42
 torch.manual_seed(seed)
 
 
-if FORMAT == 'llama-2':
+if tokenizer.chat_template:
+    # Use default chat_template
+    pass
+elif FORMAT == 'llama-2':
     tokenizer.chat_template = "{%- for idx in range(0, messages|length) -%}\n{%- if messages[idx]['role'] == 'user' -%}\n{%- if idx > 1 -%}\n{{- bos_token + '[INST] ' + messages[idx]['content'] + ' [/INST]' -}}\n{%- else -%}\n{{- messages[idx]['content'] + ' [/INST]' -}}\n{%- endif -%}\n{% elif messages[idx]['role'] == 'system' %}\n{{- bos_token + '[INST] <<SYS>>\\n' + messages[idx]['content'] + '\\n<</SYS>>\\n\\n' -}}\n{%- elif messages[idx]['role'] == 'assistant' -%}\n{{- ' '  + messages[idx]['content'] + ' ' + eos_token -}}\n{% endif %}\n{% endfor %}\n"
 elif FORMAT == 'tess':
 	tokenizer.chat_template = "{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}{% for message in messages %}{{message['role'].upper() + ': ' + message['content'] + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ 'ASSISTANT: ' }}{% endif %}"
@@ -118,9 +147,23 @@ else:
     # default to chatml
     tokenizer.chat_template = "{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
 
-
 # system, user, assistant
 chat = [{"role": "system", "content": PROMPT}]
+
+
+
+# Key bindings for toggling between single-line and multiline input modes
+key_bindings = KeyBindings()
+
+@key_bindings.add('c-j')
+def _(event):
+    event.app.current_buffer.multiline = not event.app.current_buffer.multiline()
+
+'''
+@key_bindings.add(Keys.ControlEnter)
+def _(event):
+    event.app.current_buffer.validate_and_handle()
+'''
 
 def chat_with_model():
     # updatable globals
@@ -136,22 +179,27 @@ def chat_with_model():
 
     while True:
         # Get input from the user
-        user_input = input("User: ")
+        user_input = prompt("User: ", multiline=True, key_bindings=key_bindings)
         if user_input.lower() == 'exit':
             break
         elif user_input[0] == '/':
             command, value = (user_input.split() + [None])[:2]
             if command == '/temp':
                 temp = float(value)
+                print(f"{command[1:]} set to: {value}")
             elif command == '/rep':
                 rep = float(value)
+                print(f"{command[1:]} set to: {value}")
             elif command == '/top_p':
                 top_p = float(value)
+                print(f"{command[1:]} set to: {value}")
             elif command == '/max':
                 maxt = int(value)
+                print(f"{command[1:]} set to: {value}")
             elif command == '/exit':
                 break
             elif (command == '/clear' or command == '/reset'):
+                print('Resetting context...')
                 chat = [{"role": "system", "content": PROMPT}]
             elif command == '/prompt':
                 if not value:
@@ -159,6 +207,7 @@ def chat_with_model():
                 else:
                     PROMPT = user_input.split('/prompt')[1]
                     chat[0]['content'] = PROMPT
+                    print(f"New prompt: {PROMPT}")
             else:
                 print("valid settings are: /temp /rep /top_p")
             continue
